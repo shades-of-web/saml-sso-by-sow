040fee8bb1658f0fed80e3a437c7ce28aa5c0384
Akash Mole <akash.mole@shadesofweb.com>
2024-03-09 14:06:59 +0530

    wp-load

--- saml-sso-acs.php
+++ saml-sso-acs.php
@@ -6,11 +6,31 @@ use OneLogin\Saml2\Auth;
 
 use SAMLSSO\SamlSsoConfig;
 
+
+// Dynamic path resolution to find the WordPress environment
+$wp_load_path = __DIR__; // Start with the current directory.
+$found = false; // Flag to determine if wp-load.php has been found.
+
+// Loop to move up in the directory hierarchy and check for wp-load.php
+while (!$found && $wp_load_path != '/' && is_dir($wp_load_path)) {
+  $potential_wp_load = $wp_load_path . '/wp-load.php';
+  if (file_exists($potential_wp_load)) {
+    require_once($potential_wp_load);
+    $found = true; // Set found to true to exit the loop.
+    break; // Exit the loop since we've found wp-load.php
+  }
+  $wp_load_path = dirname($wp_load_path); // Move one directory up.
+}
+
+if (!$found) {
+  exit('Failed to find wp-load.php. Please check the path and try again.');
+}
+
+
 /**
  * Load the WordPress environment and SAML configuration files.
  */
-include_once dirname(__DIR__, 3) . '/wp-load.php';
-include_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 /**
  * Define SAML settings from WordPress options.
